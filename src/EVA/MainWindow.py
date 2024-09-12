31583342bd1a9c819372e9515db6aa1b58d97525
Matea-STFC <tea.bjorklund@stfc.ac.uk>
2024-09-12 10:03:46 +0100

    move program entry point to main.py

--- src/EVA/MainWindow.py
+++ src/EVA/MainWindow.py
@@ -624,12 +624,3 @@ class MainWindow(QMainWindow):
 
     lg.loadgamma()
     print('loading gamma end',time.time())
-
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    app.setStyleSheet("QLabel{font-size: 8pt;}"
-                      "QLineEdit{font-size: 8pt;}"
-                      "QPushButton{font-size: 8pt;}")
-    mainWin = MainWindow()
-    mainWin.show()
-    sys.exit(app.exec())
