78424b803d5683f74064a916c9f90445bcfb11f0
Matea-STFC <tea.bjorklund@stfc.ac.uk>
2024-09-12 16:42:51 +0100

    simplify plotwidget class

--- src/EVA/PlotWidget.py
+++ src/EVA/PlotWidget.py
@@ -1,39 +1,28 @@
 import matplotlib
+from matplotlib import pyplot as plt
 from matplotlib.backends.backend_qtagg import FigureCanvasQTAgg
-from matplotlib.backends.backend_qtagg import NavigationToolbar2QT
+
+from PyQt6.QtCore import QSize
 from PyQt6.QtWidgets import (
     QWidget,
-    QVBoxLayout
+    QSizePolicy,
+    QStyle
 )
 
 matplotlib.use("QtAgg")
 
-"""
-    PlotWidget inherits from FigureCanvasQTAgg which allows it to be embedded into Qt, but it has limited functionality.
-    To make it easier to work, it also inherits from QWidget.
-"""
 
-
-class PlotWidget(FigureCanvasQTAgg, QWidget):
-    def __init__(self, fig=None, axs=None, plt=None, *args, **kwargs):
+class PlotWidget(FigureCanvasQTAgg):
+    def __init__(self, fig=None, axs=None):
         super(FigureCanvasQTAgg, self).__init__(fig)
-        super(QWidget).__init__(*args, **kwargs)
-
-        self.fig = fig
         self.axs = axs
-        self.plt = plt
+        self.setStyleSheet("background-color: pink")
 
-        self.layout = QVBoxLayout()
-        self.setLayout(self.layout)
-
-    def update_plot(self, plt=None, axs=None, fig=None):
+    def update_plot(self, fig=None, axs=None):
         if fig is not None:
-            self.fig = fig
+            self.figure = fig
 
         if axs is not None:
             self.axs = axs
-
-        if plt is not None:
-            self.plt = plt
 
         self.draw()
