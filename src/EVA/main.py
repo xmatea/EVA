178c06baaf082734723213d27c16ee2e1d3adfd7
Matea-STFC <tea.bjorklund@stfc.ac.uk>
2024-09-18 15:02:06 +0100

    move program entry point to main.py

--- src/EVA/main.py
+++ src/EVA/main.py
@@ -1,12 +1,14 @@
 import sys
 from PyQt6.QtWidgets import QApplication
 from MainWindow import MainWindow
+from EVA import config
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     app.setStyleSheet("QLabel{font-size: 8pt;}"
                       "QLineEdit{font-size: 8pt;}"
                       "QPushButton{font-size: 8pt;}")
+    config.init()
     mainWin = MainWindow()
     mainWin.show()
     sys.exit(app.exec())
