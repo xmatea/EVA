160ebcb799ad4a40981388ff121c07528d4dc303
Matea-STFC <tea.bjorklund@stfc.ac.uk>
2024-09-27 16:40:24 +0100

    create App instance of custom "app" class rather than QApplication

--- src/EVA/main.py
+++ src/EVA/main.py
@@ -2,13 +2,14 @@ import sys
 from PyQt6.QtWidgets import QApplication
 from MainWindow import MainWindow
 from EVA import config
+from EVA.app import App
 
 if __name__ == "__main__":
-    app = QApplication(sys.argv)
+    app = App(sys.argv)
     app.setStyleSheet("QLabel{font-size: 8pt;}"
                       "QLineEdit{font-size: 8pt;}"
                       "QPushButton{font-size: 8pt;}")
-    config.init()
-    mainWin = MainWindow()
-    mainWin.show()
+
+    app.main_window = MainWindow()
+    app.main_window.show()
     sys.exit(app.exec())
