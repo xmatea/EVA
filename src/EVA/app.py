ced23e9d823e551e39ee7101ea6c6f5b6e0ee661
Matea-STFC <tea.bjorklund@stfc.ac.uk>
2024-09-27 16:39:43 +0100

    create App class

---
+++ src/EVA/app.py
@@ -0,0 +1,38 @@
+from PyQt6.QtWidgets import QApplication
+
+from EVA import config, MainWindow
+
+
+def get_app():
+    return QApplication.instance()
+
+
+def get_config():
+    return get_app().config
+
+
+class App(QApplication):
+    """
+    The app class contains all settings, parameters, etc. of the app. It has a single instance (created in main.py)
+    which can be accessed anywhere using QApplication.instance(). The instance can easily be returned using the
+    get_app() method defined above.
+    """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # A reference to all windows will be stored within the app class
+        self.main_window = MainWindow.MainWindow()
+        self.multiplot_window = None
+        self.manual_window = None
+        self.peakfit_window = None
+        self.plot_window = None
+        self.srim_window = None
+
+        # store config in app
+        self.config = config.Config()
+
+        # create dataloader
+        self.data_loader = None
+
+
+
