ab443948bb93c2e97fb04d35c95213cd03c944f7
Matea-STFC <tea.bjorklund@stfc.ac.uk>
2024-09-27 11:42:26 +0100

    add documentation

--- src/EVA/plot_widget.py
+++ src/EVA/plot_widget.py
@@ -1,8 +1,6 @@
 import matplotlib
-from matplotlib import pyplot as plt
 from matplotlib.backends.backend_qtagg import FigureCanvasQTAgg, NavigationToolbar2QT
-
-from PyQt6.QtCore import QSize, Qt
+from PyQt6.QtCore import Qt
 from PyQt6.QtWidgets import (
     QWidget,
     QVBoxLayout
@@ -10,31 +8,48 @@ from PyQt6.QtWidgets import (
 
 matplotlib.use("QtAgg")
 
+
 class FigureCanvas(FigureCanvasQTAgg):
+    """
+    This is the class that interacts with matplotlib and does all the plot rendering.
+    """
     def __init__(self, fig=None, axs=None):
         super().__init__(fig)
         self.axs = axs
 
-    def __del__(self):
-        print("deleted")
 
 class PlotWidget(QWidget):
+    """
+    This is a "wrapper" widget class that contains both the navigation bar and the figure canvas, to avoid having to
+    link the two manually every time a PlotWidget is needed. It also takes care of figure resizing and linking the
+    navbar and plot together.
+    """
     def __init__(self, fig=None, axs=None):
         super().__init__()
-
+        # create the figure canvas
         self.canvas = FigureCanvas(fig=fig, axs=axs)
-        self.layout = QVBoxLayout()
 
+        # this avoids having a navbar when there is no figure
         if fig is None:
             self.navbar = None
         else:
             self.navbar = NavigationToolbar2QT(self.canvas, self)
 
+        # add navbar and plot canvas to layout
+        self.layout = QVBoxLayout()
         self.layout.addWidget(self.navbar, Qt.AlignmentFlag.AlignLeft)
         self.layout.addWidget(self.canvas)
         self.setLayout(self.layout)
 
     def update_plot(self, fig=None, axs=None):
+        """
+        For a simple axes update, it is enough to update the axs parameter of the canvas and redraw.
+
+        When updating the figure of the plot, the navbar will un-link and the figure will not be resized to fit the
+        size of the container. To re-link the nav bar and force a size update, the navbar and canvas is removed from
+        the layout, deleted, then re-initialised and re-added to widget.
+        """
+
         if axs is not None:
             self.canvas.axs = axs
             self.canvas.draw()
@@ -42,19 +57,16 @@ class PlotWidget(QWidget):
         if fig is not None:
             self.canvas.figure = fig
 
-            """
-            To ensure the navigation bar is re-linked to the new figure, and that the figure is resized to the fit the
-            size of the widget, the navbar and canvas is removed from the layout, deleted, then re-initialised and
-            re-added to widget.
-            """
-
+            # Since canvas is removed from widget and has no parent, 'deleteLater()' will delete it immediately.
             self.layout.removeWidget(self.canvas)
             self.canvas.deleteLater()
 
+            # Only delete navbar if it is not None, else it will error
             if self.navbar is not None:
                 self.layout.removeWidget(self.navbar)
                 self.navbar.deleteLater()
 
+            # Create new navbar and figure canvas, link them and add them back into widget layout
             self.canvas = FigureCanvas(fig=self.canvas.figure, axs=self.canvas.axs)
             self.navbar = NavigationToolbar2QT(self.canvas, self)
 
