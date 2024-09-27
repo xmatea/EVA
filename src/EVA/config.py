4a9cc0d5e9b77741f0e0f73962623e075a6758c1
Matea-STFC <tea.bjorklund@stfc.ac.uk>
2024-09-27 16:39:23 +0100

    rewrite config.py as class

--- src/EVA/config.py
+++ src/EVA/config.py
@@ -1,14 +1,23 @@
 from configparser import ConfigParser
-parser = ConfigParser()
-def init():
-    parser.read("./src/EVA/config.ini")
-def restore_defaults():
-    default_parser = ConfigParser()
-    default_parser.read("./src/EVA/defaults.ini")
-    for section in default_parser:
-        parser[section] = default_parser[section]
 
-def save_config():
-    with open("./src/EVA/config.ini", "w") as config_file:
-        parser.write(config_file)
-        config_file.close()
+
+class Config:
+    """
+    The config class uses the built-in module configparser to read and write configurations as .ini files.
+    """
+    def __init__(self):
+        self.parser = ConfigParser()
+
+    def load(self):
+        self.parser.read("./src/EVA/config.ini")
+
+    def save_config(self):
+        with open("./src/EVA/config.ini", "w") as config_file:
+            self.parser.write(config_file)
+            config_file.close()
+
+    def restore_defaults(self):
+        default_parser = ConfigParser()
+        default_parser.read("./src/EVA/defaults.ini")
+        for section in default_parser:
+            self.parser[section] = default_parser[section]
