62d4e9d561ed7403df96db9f053facaa862cbfdb
Matea-STFC <tea.bjorklund@stcf.ac.uk>
2024-10-14 15:52:55 +0100

    use new data classes

--- src/EVA/Energy_Corrections.py
+++ src/EVA/Energy_Corrections.py
@@ -1,27 +1,19 @@
+import numpy as np
 from EVA.app import get_config
-from EVA.loaddata import Dataset
+from EVA.data_structures import Dataset
 
 def Energy_Corrections(data):
     config = get_config()
 
-    corrected = []
     # For each dataset in the list, return energy corrected data if energy correction is wanted
     for dataset in data:
         detector = dataset.detector
         if config.parser[detector]["use_energy_correction"]:
             gradient = config.parser[detector]["e_corr_gradient"]
             offset = config.parser[detector]["e_corr_offset"]
-            xdata = lincorr(dataset.x, gradient, offset)
-        else:
-            xdata = dataset.x
-
-        # Create new datasets
-        corrected.append(Dataset(xdata, dataset.y, detector))
-
-    return corrected
-
+            dataset.x = lincorr(dataset.x, gradient, offset)
+    return data
 
 def lincorr(x, m, c):
     new_x = x*float(m)+float(c)
-
     return new_x
--- src/EVA/Normalise.py
+++ src/EVA/Normalise.py
@@ -1,13 +1,30 @@
 import numpy as np
+from EVA.data_structures import Dataset
 
+def normalise(data, events, normalisation):
+    # checks which normalisation is set in config
+    if normalisation == "counts":
+        data, norm_flag = normalise_counts(data)
+    elif normalisation == "events":
+        data, norm_flag = normalise_events(data, events)
+    else:
+        norm_flag = 0 # do nothing if normalisation is not needed
+
+    return data, norm_flag
 
 def normalise_counts(data):
-    normalised = []
     for dataset in data:
+        dataset.y = dataset.y / np.sum(dataset.y) * pow(10, 5)
 
-    return dataset.y / np.sum(dataset.y) * pow(10, 5)
+    return data, 0
 
+def normalise_events(data, events_str):
+    if events_str == " ": # default value of events_str if the comment is not loaded
+        return data, 1
 
-def normalise_spill(dataset, events_str):
     spills = int(events_str[19:])
-    return dataset.y / spills * pow(10, 5)
+
+    for dataset in data:
+        dataset.y = dataset.y / spills * pow(10, 5)
+
+    return data, 0
