7e8db96245ca995d0decc586b153138118ccd56f
Matea-STFC <tea.bjorklund@stcf.ac.uk>
2024-11-05 11:40:50 +0000

    move muon database selection to app.py

--- src/EVA/main.py
+++ src/EVA/main.py
@@ -1,7 +1,5 @@
 import sys
-from PyQt6.QtWidgets import QApplication
 from MainWindow import MainWindow
-from EVA import config, loadgamma, LoadDatabaseFile
 from EVA.app import App
 
 if __name__ == "__main__":
@@ -9,18 +7,6 @@ if __name__ == "__main__":
     app.setStyleSheet("QLabel{font-size: 8pt;}"
                       "QLineEdit{font-size: 8pt;}"
                       "QPushButton{font-size: 8pt;}")
-
-    # load energy databases
-    app.gamma_database = loadgamma.load_gamma_data()
-
-    # set muon database as specified in config
-    if app.config["database"]["mu_xray_db"] == "legacy":
-        app.muon_database = LoadDatabaseFile.load_legacy_data()
-
-    elif app.config["database"]["mu_xray_db"] == "mudirac":
-        app.muon_database = LoadDatabaseFile.load_mudirac_data()
-    else:
-        raise ValueError()
     
     app.main_window = MainWindow()
     app.main_window.show()
