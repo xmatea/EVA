9ec833ccd3fc7f3600891934b9cf3841182b0acf
Matea-STFC <tea.bjorklund@stcf.ac.uk>
2024-11-19 10:50:58 +0000

    move windows to separate folder

--- src/EVA/windows/energy_correction_window.py
+++ src/EVA/windows/energy_correction_window.py
@@ -7,7 +7,7 @@ from PyQt6.QtWidgets import (
     QGridLayout, QMessageBox,
 )
 
-from EVA.app import get_app, get_config
+from EVA.classes.app import get_app, get_config
 
 class Correction_E(QWidget):
     """
